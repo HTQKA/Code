c83a143a37e3637c3be65e48a3c43baa436e2db4
HTQKA <2811107845@qq.com>
2020-09-30 08:11:24 +0800

    File文件测试代码

--- src/知识点/IO流/FileTest.java
+++ src/知识点/IO流/FileTest.java
@@ -1,8 +1,55 @@
 package 知识点.IO流;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * @author XYSIMJ
  * @create 2020-09-21-下午7:41
  */
 public class FileTest {
+
+    @Test
+    public void testFile() {
+        File file = new File("a");
+        System.out.println(file.getAbsolutePath());//获取绝对路径
+        System.out.println(file.getPath());//获取相对路径
+//        Fileile file1 = new File("xys");
+//        file.renameTo(file1);
+        System.out.println(file.getName());
+    }
+
+    //创建文件夹
+    @Test
+    public void test() {
+        File dir = new File("xys.txt");
+        if (!dir.exists()){
+            dir.mkdir();
+        }
+    //以dir为父目录,创建子目录dir2的File对象
+        File dir2 = new File("xys.txt","dir2");
+        if (!dir2.exists()) {
+            dir2.mkdirs();
+        }
+
+        //
+        File dir4 = new File(dir,"dir3/dir4");
+        if (!dir4.exists()) {
+            dir4.mkdirs();
+        }
+        //创建dir2问父目录的,名为test.txt的File对象
+        File file = new File(dir2,"test.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
